d4d372ca50f7a42aa0bfed3a30a2f6dcc8475bc9
yi jiang <yi.jiang3@bosch.com>
2024-01-23 09:31:18 +0800

    ++

--- tensorrt/main.cpp
+++ tensorrt/main.cpp
@@ -22,20 +22,20 @@ int main() {
         uint32_t explicitBatch = 1U << static_cast<uint32_t>(nvinfer1::NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
         auto network = std::unique_ptr<nvinfer1::INetworkDefinition>(builder->createNetworkV2(explicitBatch));
 
-        std::string onnx_filename = "/home/icv/code/workspaces/inf_onsite/test/onnxfiles/3d_det_all.onnx";
+        std::string onnx_filename = "/home/icv/workspaces/inf_onsite/test/onnxfiles/3d_det_all.onnx";
         auto parser = std::unique_ptr<nvonnxparser::IParser>(nvonnxparser::createParser(*network, logger));
         parser->parseFromFile(onnx_filename.c_str(), static_cast<int32_t>(nvinfer1::ILogger::Severity::kWARNING));
 
         auto config = std::unique_ptr<nvinfer1::IBuilderConfig>(builder->createBuilderConfig());
+        config->setFlag(nvinfer1::BuilderFlag::kFP16);
         nvinfer1::IOptimizationProfile* profile = builder ->createOptimizationProfile();
         profile->setDimensions("input", nvinfer1::OptProfileSelector::kMIN, nvinfer1::Dims4{1, 3, 1080, 1920});
-        profile->setDimensions("input", nvinfer1::OptProfileSelector::kOPT, nvinfer1::Dims4{2, 3, 1080, 1920});  
-        profile->setDimensions("input", nvinfer1::OptProfileSelector::kMAX, nvinfer1::Dims4{3, 3, 1080, 1920});  
-
+        profile->setDimensions("input", nvinfer1::OptProfileSelector::kOPT, nvinfer1::Dims4{1, 3, 1080, 1920});
+        profile->setDimensions("input", nvinfer1::OptProfileSelector::kMAX, nvinfer1::Dims4{1, 3, 1080, 1920});
         config->addOptimizationProfile(profile);
         auto serializedModel = std::unique_ptr<nvinfer1::IHostMemory>(builder->buildSerializedNetwork(*network, *config));
 
-        std::ofstream trtFile("your_model.trt", std::ios::binary);
+        std::ofstream trtFile("your_model_fp16.trt", std::ios::binary);
         trtFile.write(static_cast<const char*>(serializedModel->data()), serializedModel->size());
         trtFile.close();
 
